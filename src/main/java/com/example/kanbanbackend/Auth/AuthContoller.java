dadfcfa727472ee1c3a757f3893954c274fcf776
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2024-11-25 15:26:57 +0700

    fix: redirect private url

--- src/main/java/com/example/kanbanbackend/Auth/AuthContoller.java
+++ src/main/java/com/example/kanbanbackend/Auth/AuthContoller.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
@@ -23,18 +24,21 @@ public class AuthContoller {
     @Autowired
     private UserService service;
 
-
+    @Value("${spring.profiles.active:dev}")
+    private String activeProfile;
     @GetMapping("")
     public void redirectToMicrosoftLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String originalUrl = request.getRequestURL().toString();
-        if (!originalUrl.contains("/sy2")) {
+        if ("prod".equalsIgnoreCase(activeProfile) && !originalUrl.contains("/sy2")) {
             String host = request.getHeader("X-Forwarded-Host");
             String protocol = request.getHeader("X-Forwarded-Proto");
             String fullUrl = protocol + "://" + host + "/sy2/api/oauth2/authorization/azure-dev";
-            response.sendRedirect(fullUrl);
-        } else {
-            response.sendRedirect("/oauth2/authorization/azure-dev");
+            if(host != null && protocol != null) {
+                response.sendRedirect(fullUrl);
+            }
         }
+        response.sendRedirect("/oauth2/authorization/azure-dev");
+
     }
 
     @PostMapping("")
